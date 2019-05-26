3849a5b1296ad94875d7bfcbd769d52d37bb1999
msoriano1 <matthew.soriano@obf.ateneo.edu>
2019-05-26 23:51:50 +0800

    topic delete2

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
     self.primary_key = 'id'
     belongs_to :yearlevel
 
-    has_many :worksheets
+    has_many :worksheets, dependent: :delete_all
     has_many :items
     has_many :answered_worksheets
     has_many :studentanswers
