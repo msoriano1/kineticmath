1c0f026b0398e738db7d1fe07b55ebf2cac330e2
msoriano1 <matthew.soriano@obf.ateneo.edu>
2019-06-06 15:00:29 +0800

    admin edit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,7 +10,29 @@ class AdminsController < ApplicationController
     def new
         @admin = Admin.new
     end
+
+    def edit   
+        @admin = Admin.find_by(id: params[:admin])
+        if @admin.nil?
+            redirect_to admins_path, alert: "Admin not found." and return
+        else
+            render "admins/edit" 
+        end
+    end
     
+    def update
+        @admin = Admin.find_by(id: params[:admin][:id])
+        if @admin.nil?
+            redirect_to admins_path, alert: "Admin not found." and return
+        else 
+            @admin.ausername = params[:admin][:ausername]
+            @admin.encrypted_password = params[:admin][:encrypted_password]
+            @admin.save!
+
+            redirect_to admins_path, notice: "Admin successfully edited."
+        end
+    end
+
     def create
         @admin = Admin.new params[:teacher]
         if @admin.save
@@ -19,4 +41,17 @@ class AdminsController < ApplicationController
             render :action => 'new'
         end
     end
+
+    private
+
+    def admin_params
+        params.require(:admin).permit(:ausername, :salt, :encrypted_password, :password_confirmation)
+      end
+      
+      def set_admin
+        @admin = Admin.find_by(id: params[:id])
+        if @admin.nil?
+              redirect_to admins_path, alert: "Admin not found." and return
+          end
+      end
 end
